9283aa60190a779564805b90fd0a32742eae0c94
Boyan Rabchev <boyan@rabchev.com>
2012-11-01 17:45:25 +0200

    Completed serving files and directories from node http server.

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+/*jslint plusplus: true, devel: true, nomen: true, node: true, indent: 4, maxerr: 50 */
+/*global require, exports, module */
+
 var express = require('express');
 var brackets = require('node-brackets');
 
